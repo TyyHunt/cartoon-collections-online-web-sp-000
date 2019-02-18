abf1192e57ea4e243c2209eea17ef8dd2e9be9d6
Tyler Hunt <tyhunt04@gmail.com>
2019-02-18 17:52:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,13 @@ def long_planeteer_calls(planeteer_calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(foods)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if foods.include?(cheese_types)
+    foods.find do |food|
+      food.is_a?(cheese_types)
+    end
+  else
+    return nil
+  end
 end
