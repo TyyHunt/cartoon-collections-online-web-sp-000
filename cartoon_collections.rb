88b6573fb5c565da7ec3034a30f449d733e778be
Tyler Hunt <tyhunt04@gmail.com>
2019-02-18 17:34:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,9 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |calls|
-    puts "#{calls}.capitalize + '!'"
+    "#{calls}.capitalize + '!'"
   end
+  
 end
 
 def long_planeteer_calls# code an argument here
